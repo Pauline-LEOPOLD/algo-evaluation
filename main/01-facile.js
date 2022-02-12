dc050f05ee0958c238ba3cd8332483dc561e3fb3
Pauline-LEOPOLD <pauline.leopold2002@gmail.com>
2022-02-12 18:35:37 +0000

    <algoevaluationpauline>

--- main/01-facile.js
+++ main/01-facile.js
@@ -19,6 +19,18 @@ console.log (signe (56))
  * Programmer une fonction prenant en argument un tableau de nombres, et qui renvoie un tableau ne contenant que les nombres pairs.
  * Astuce : un nombre pair est un nombre dont le reste de la division par 2 est 0.
  */
-function nombresPairs(nombres) {
-
+function nombresPairs(nombre) {
+    let tableauResult = [];
+    let tableauFinal = 0;
+    let nombres = 0;
+    while (nombre < nombre.length) {
+        if (nombre[nombres] % 2 === 0) {
+            tableauResult[tab] = nombre[nombres];
+            tab++;
+        }
+        nombres++
+    }
+    return tableauResult
 }
+let tabComplet = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+console.log(nombresPairs(tabComplet))
--- main/02-intermediaire.js
+++ main/02-intermediaire.js
@@ -4,8 +4,17 @@
  * Exemple : [1, 2, 3] et ["apple", "orange", "banana"] renverra : [1, "apple", 2, "orange", 3, "banana"]
  */
 function combine(tableau1, tableau2) {
-
+    var firstTab =[];
+    if (tableau1.length !== tableau2.length) return
+    for (var i = 0; i < tableau1.length; i++) {
+        firstTab[i*2] = tableau1[i];
+        firstTab[i*2+1]= tableau2[i];
+    }
+    return firstTab;
 }
+var tableau1Combine = [1, 2, 3, 4, 5, 6];
+var taleau2Combine = ['orange', 'apple', 'strawberry', 'banana','pear','pineapple'];
+console.log(combine(tableau1Combine, tableau2Combine))
 
 /**
  * Programmer une fonction prenant en argument un tableau d'éléments et une valeur offset, et qui renvoie un tableau avec les valeurs du tableau en argument décalées de la valeur
